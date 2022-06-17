a32ae23af4541b2ba35fdd567cb8aa73bc0cda63
er.hyun <ryul3024@gmail.com>
2022-06-17 10:29:15 +0900

    exposed연습
    사용 시, 객체지향적보다 실제 모델을 생각하며 구현해야 함

--- src/main/kotlin/jpabook/jpashop2/domain/Order.kt
+++ src/main/kotlin/jpabook/jpashop2/domain/Order.kt
@@ -1,16 +1,14 @@
 package jpabook.jpashop2.domain
 
-import jpabook.jpashop2.domain.Category.Companion.referrersOn
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.IntIdTable
-import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.javatime.date
 
-object Orders: IntIdTable() {
-    val member = reference("member", Members)
-    val delivery = reference("delivery", Deliveries)
+object OrderTable: IntIdTable() {
+    val member = reference("member", MemberTable)
+    val delivery = reference("delivery", DeliveryTable)
     val orderDate = date("order_date")
     val status = enumeration<OrderStatus>("order_status")
 }
@@ -21,9 +19,9 @@ enum class OrderStatus(var status: Int) {
 }
 
 class Order(id: EntityID<Int>): IntEntity(id) {
-    companion object: IntEntityClass<Order>(Orders)
-    var member by Member referencedOn Orders.member
-    var delivery by Delivery referencedOn Orders.delivery
-    var orderDate by Orders.orderDate
-    var status by Orders.status
+    companion object: IntEntityClass<Order>(OrderTable)
+    var member by Member referencedOn OrderTable.member
+    var delivery by Delivery referencedOn OrderTable.delivery
+    var orderDate by OrderTable.orderDate
+    var status by OrderTable.status
 }
--- src/test/kotlin/jpabook/jpashop2/DomainTest.kt
+++ src/test/kotlin/jpabook/jpashop2/DomainTest.kt
@@ -1,7 +1,7 @@
 package jpabook.jpashop2
 
-import jpabook.jpashop2.domain.Member
-import jpabook.jpashop2.domain.Members
+import jpabook.jpashop2.domain.*
+import jpabook.jpashop2.domain.item.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
@@ -18,20 +18,68 @@ class DomainTest {
         Database.connect("jdbc:h2:tcp://localhost/~/jpashop", driver = "org.h2.Driver", user = "sa", password = "")
 
         transaction {
-            SchemaUtils.create(Members)
+            SchemaUtils.create(MemberTable)
 
-            val stPeteId = Members.insert {
+            val stPeteId = MemberTable.insert {
                 it[name] = "St. Petersburg"
-                it[address] = "우리집어딘가"
-            } get Members.id
+                it[city] = "서울"
+                it[street] = "앞길"
+                it[zipcode] = "08217"
+            } get MemberTable.id
 
             val stPeteId2 = Member.new {
                 name = "the last jedi"
-                address = "우리집은 일단 아님"
+                city = "서울"
+                street = "앞길"
+                zipcode = "08217"
             }
 
             // 'select *' SQL: SELECT Cities.id, Cities.name FROM Cities
-            println("Cities: ${Members.selectAll()}")
+            println("Cities: ${MemberTable.selectAll().count()}")
+        }
+    }
+
+    @Test
+    fun `상속 확인 테스트`() {
+        Database.connect("jdbc:h2:tcp://localhost/~/jpashop", driver = "org.h2.Driver", user = "sa", password = "")
+
+        transaction {
+            SchemaUtils.create(ItemTable)
+            SchemaUtils.create(BookTable)
+            SchemaUtils.create(AlbumTable)
+            SchemaUtils.create(MovieTable)
+
+            val item = Item.new {
+                name = "the last jedi"
+                price = 10000
+                stockQuantity = 30
+            }
+
+
+            val book = Book.new {
+                author = "me"
+                isbn = "dadada"
+                itemId = item.id
+            }
+
+            val album = Album.new {
+                artist = "me"
+                itemId = item.id
+            }
+
+            val movie = Movie.new {
+                actor = "me"
+                director = "me"
+                itemId = item.id
+            }
+
+            /*BookTable.insert {
+                it[author] = "me"
+                it[isbn] = "dadaa"
+                it[id] = item.id
+            }*/
+
+            println(ItemTable.selectAll().toMutableList())
         }
     }
 }
